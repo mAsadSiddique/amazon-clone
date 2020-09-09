182c7980397ef25d9b54af04b9f79099394717c9
mAsadSiddique <asadsiddique.a@gmail.com>
2020-09-09 16:35:31 +0500

    Add Product Component and Customize CSS All other Components

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,29 +1,46 @@
 import React from 'react';
-import pic from './Fuji_TallHero_45M_v2_1x._CB432458380_.jpg'
 import "./Home.css";
+import Product from '../Product/Product';
 
 function Home() {
     return (
         <div className="home">
             <div className="home__container">
-                <img src={pic}
+                <img
+                    className="home__image"
+                    src="https://images-eu.ssl-images-amazon.com/images/G/02/digital/video/merch2016/Hero/Covid19/Generic/GWBleedingHero_ENG_COVIDUPDATE__XSite_1500x600_PV_en-GB._CB428684220_.jpg"
                     alt="home" />
             </div>
 
             <div className="home__row">
-                {/* Product */}
-                {/* Product */}
+                <Product
+                    title="The Lean Startup"
+                    price={29.99}
+                    image="https://images-na.ssl-images-amazon.com/images/I/51mDUtXlJzL.jpg"
+                    rating={3}
+                />
+                <Product
+                    id="49538094"
+                    title="Kenwood kMix Stand Mixer for Backing, Stylish Kitchen Mixer with K-beater, Dough Hook and Whisk, 5 Liter Glass Bowl"
+                    price={2390.0}
+                    image="https://images-na.ssl-images-amazon.com/images/I/810%2BGNdkzKL._AC_SX450_.jpg"
+                    rating={4}
+                />
             </div>
 
             <div className="home__row">
-                {/* Product */}
-                {/* Product */}
-                {/* Product */}
+                <Product
+                    title="Sumsung LC49RG90SSUXEN 49' Curved LED Gaming Monitor"
+                    price={199.99}
+                    image="https://images-na.ssl-images-amazon.com/images/I/71Swqqe7XAL._AC_SX566_.jpg"
+                    rating={3} />
+                <Product />
+                <Product />
             </div>
 
             <div className="home__row">
-                {/* Product */}
-                
+                <Product />
+
             </div>
 
         </div>
---
+++ src/Components/Product/Product.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import './Product.css';
+
+function Product({ id,title, image, price,rating}) {
+    return (
+        <div className="product">
+            <div className="product__info">
+                <p>{title}</p>
+                <p className="product__price">
+                    <small>$</small>
+                    <strong>{price}</strong>
+                </p>
+                <div className="product__rating">
+                    {Array(rating).fill().map((_, i)=>{
+                        return(
+                            <div key={i}>
+                            <p><span class="fa fa-star checked"></span></p>
+                            </div>
+                        )
+                    })}
+                   
+                </div>
+            </div>
+
+            <img src={image}
+                alt=""
+            />
+
+            <button>Add to Basket</button>
+
+        </div>
+    );
+}
+
+export default Product
